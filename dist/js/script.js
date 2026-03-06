ef554cc049cc28ffbf02d1fa34835bdc87380c9d
Tomasz Osuch <tomasz.osuch@gmail.com>
2026-03-06 02:32:43 +0100

    add(): add initAccordion method

--- dist/js/script.js
+++ dist/js/script.js
@@ -53,18 +53,19 @@
   };
 
   class Product {
-    constructor(id, data) {
+    constructor(id, data){
       const thisProduct = this;
 
       thisProduct.id = id;
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
-      
+      thisProduct.initAccordion();
+
       console.log('new Product:', thisProduct);
     }
 
-    renderInMenu() {
+    renderInMenu(){
       const thisProduct = this;
 
       /* generate HTML based on template */
@@ -73,15 +74,36 @@
       /* create element using utils.createDOMFromHTML */
       thisProduct.element = utils.createDOMFromHTML(generatedHTML);
 
-      /* find menu container */
+      // find menu container
       const menuContainer = document.querySelector(select.containerOf.menu);
-      
-      /* add element to menu */
+
+      // add element to menu
       menuContainer.appendChild(thisProduct.element);
 
     }
 
-    
+    initAccordion(){
+      const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+      /* prevent default action for event */
+      event.preventDefault();
+      /* find active product (product that has active class) */
+      const activeProduct = document.querySelector(
+        select.all.menuProductsActive
+      );
+      /* if there is active product and it's not thisProduct.element, remove class active from it */
+      if (activeProduct && activeProduct !== thisProduct.element) {
+        activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+      }
+      /* toggle active class on thisProduct.element */
+      thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+      });
+    }
   }
 
   const app = {
