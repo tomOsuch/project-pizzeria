1739120b26a937b018361796fc45e2980a0b546e
Tomasz Osuch <tomasz.osuch@gmail.com>
2026-03-25 12:46:58 +0100

    add(js): add class Card and showing and hiding the shopping cart

--- dist/js/script.js
+++ dist/js/script.js
@@ -316,6 +316,40 @@
     }
   }
 
+  class Cart {
+    constructor(element) {
+      const thisCart = this;
+
+      thisCart.products = [];
+
+      thisCart.getElements(element);
+      thisCart.initActions();
+
+      console.log('new cart: ', thisCart);
+    }
+
+    getElements(element) {
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions() {
+      const thisCart = this;
+
+      const triggerCart = thisCart.dom.toggleTrigger;
+
+      triggerCart.addEventListener('click', function (event) {
+        event.preventDefault();
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      })
+
+    }
+  }
+
   const app = {
     initMenu: function () {
       const thisApp = this;
@@ -325,16 +359,24 @@
       }
     },
 
+    initData: function () {
+      const thisApp = this;
+      thisApp.data = dataSource;
+    },
+
+    initCart: function () {
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
+
     init: function () {
       const thisApp = this;
 
       thisApp.initData();
       thisApp.initMenu();
-    },
-
-    initData: function () {
-      const thisApp = this;
-      thisApp.data = dataSource;
+      thisApp.initCart();
     },
   };
 
