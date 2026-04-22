e7f35c1d2e1aad26db1e8608dcece2c6e7c706b3
Tomasz Osuch <tomasz.osuch@gmail.com>
2026-04-22 12:42:57 +0200

    fix(): fix home page -  carousel and CTA

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -4,12 +4,15 @@ class Home {
   constructor(element) {
     this.dom = {};
     this.dom.wrapper = element;
+    this.testimonialAutoplayDelay = 5000;
+    this.testimonialAutoplayTimer = null;
 
     if (!this.dom.wrapper) {
       return;
     }
 
     this.render();
+    this.initActions();
     this.initTestimonial();
   }
 
@@ -20,11 +23,38 @@ class Home {
     thisHome.dom.wrapper.innerHTML = generatedHTML;
   }
 
+  initActions() {
+    const thisHome = this;
+    const ctaTiles = thisHome.dom.wrapper.querySelectorAll('.home-cta__tile');
+
+    for (let tile of ctaTiles) {
+      tile.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const href = this.getAttribute('href') || '';
+        const targetPageId = href.replace(/^#\/?/, '');
+
+        if (!targetPageId) {
+          return;
+        }
+
+        const targetHash = '#/' + targetPageId;
+        if (window.location.hash === targetHash) {
+          window.dispatchEvent(new Event('hashchange'));
+          return;
+        }
+
+        window.location.hash = targetHash;
+      });
+    }
+  }
+
   initTestimonial() {
     const thisHome = this;
 
     thisHome.dom.track = thisHome.dom.wrapper.querySelector('.home-testimonial__track');
     thisHome.dom.dots = thisHome.dom.wrapper.querySelectorAll('.home-testimonial__dot');
+    thisHome.dom.testimonial = thisHome.dom.wrapper.querySelector('.home-testimonial');
     thisHome.slides = thisHome.dom.track ? thisHome.dom.track.children : [];
 
     if (!thisHome.dom.track || thisHome.slides.length === 0 || thisHome.dom.dots.length === 0) {
@@ -34,6 +64,8 @@ class Home {
     thisHome.activeSlideIndex = 0;
     thisHome.bindDotEvents();
     thisHome.goToSlide(0);
+    thisHome.startAutoplay();
+    thisHome.bindAutoplayPauseOnHover();
   }
 
   bindDotEvents() {
@@ -44,9 +76,50 @@ class Home {
         const index = parseInt(this.getAttribute('data-index'), 10);
         if (!isNaN(index)) {
           thisHome.goToSlide(index);
+          thisHome.restartAutoplay();
         }
       });
     }
+  }
+
+  bindAutoplayPauseOnHover() {
+    const thisHome = this;
+
+    if (!thisHome.dom.testimonial) {
+      return;
+    }
+
+    thisHome.dom.testimonial.addEventListener('mouseenter', function () {
+      thisHome.stopAutoplay();
+    });
+
+    thisHome.dom.testimonial.addEventListener('mouseleave', function () {
+      thisHome.startAutoplay();
+    });
+  }
+
+  startAutoplay() {
+    const thisHome = this;
+
+    thisHome.stopAutoplay();
+    thisHome.testimonialAutoplayTimer = setInterval(function () {
+      thisHome.goToSlide(thisHome.activeSlideIndex + 1);
+    }, thisHome.testimonialAutoplayDelay);
+  }
+
+  stopAutoplay() {
+    const thisHome = this;
+
+    if (thisHome.testimonialAutoplayTimer) {
+      clearInterval(thisHome.testimonialAutoplayTimer);
+      thisHome.testimonialAutoplayTimer = null;
+    }
+  }
+
+  restartAutoplay() {
+    const thisHome = this;
+
+    thisHome.startAutoplay();
   }
 
   updateActiveDot() {
